26866356d054f9caf11015d1d9ddb4f62b80c0dc
vkang <vitaliykang@gmail.com>
2024-04-20 19:20:16 +0600

    V0.1

--- src/main/java/com/example/birthdatecalculator/Main.java
+++ src/main/java/com/example/birthdatecalculator/Main.java
@@ -3,6 +3,7 @@ package com.example.birthdatecalculator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class Main extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 400, 400);
         stage.setTitle("Нумерологический калькулятор");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
         stage.show();
         stage.setResizable(false);
     }
